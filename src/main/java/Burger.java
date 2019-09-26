0805af0b80f092c0296a5a3d30df8f2e6975a871
KonAnd13 <a.mailbox13@gmail>
2019-09-26 09:28:31 +0300

    changed field types and added id field. This closes #3, closes #2

--- src/main/java/Burger.java
+++ src/main/java/Burger.java
@@ -1,16 +1,17 @@
 public class Burger {
+    String id;
     String name;
     String description;
     String picture;
-    String price;
-    String weight;
-    String calories;
-    String protein;
-    String fats;
-    String carbohydrates;
+    int price;
+    int weight;
+    int calories;
+    double protein;
+    double fats;
+    double carbohydrates;
 
     @Override
     public String toString() {
-        return name + "\t" + price + " Ꝑ\n\n" + description + "\n\n" + weight + " Г   " + calories + " Ккал\nБ: " + protein + " г\tЖ: " + fats + " г\tУ: " + carbohydrates + " г\n\n";
+        return "Ссылка на " + name + ": " + id + "\n\n" + name + "\t" + price + " Ꝑ\n\n" + description + "\n\n" + weight + " Г   " + calories + " Ккал\nБ: " + protein + " г\tЖ: " + fats + " г\tУ: " + carbohydrates + " г\n\n";
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,33 @@
 public class Main {
     public static void main(String[] args) {
         Burger chizburger = new Burger();
+        chizburger.id = "https://www.kfc.ru/foodorder/product/2336";
         chizburger.name = "Чизбургер";
         chizburger.description = "Пряный горчичный соус, кетчуп, сочные стрипсы в\n" +
                 "оригинальной панировке, лук, сыр Чеддер, огурцы на\n" +
                 "пшеничной булочке с кукурузной посыпкой.";
         chizburger.picture = "https://s82079.cdn.ngenix.net/9VW3RQfY33AnJi7Yu38nKiHc.png?dw=230";
-        chizburger.price = "69";
-        chizburger.weight = "167";
-        chizburger.calories = "237";
-        chizburger.protein = "13.2";
-        chizburger.fats = "8.6";
-        chizburger.carbohydrates = "26.6";
+        chizburger.price = 69;
+        chizburger.weight = 167;
+        chizburger.calories = 237;
+        chizburger.protein = 13.2;
+        chizburger.fats = 8.6;
+        chizburger.carbohydrates = 26.6;
 
         System.out.println(chizburger);
 
         Burger longerBBQ = new Burger();
+        longerBBQ.id = "https://www.kfc.ru/foodorder/product/1018";
         longerBBQ.name = "Лонгер BBQ";
         longerBBQ.description = "Сочная курочка, маринованные огурчики, лук и аппетитный соус\n" +
                 "барбекю! Спешите попробовать!";
         longerBBQ.picture = "https://s82079.cdn.ngenix.net/sRrSzWpf86c4hMMwCJ7aHBph.png?dw=230";
-        longerBBQ.price = "50";
-        longerBBQ.weight = "107";
-        longerBBQ.calories = "236";
-        longerBBQ.protein = "9.7";
-        longerBBQ.fats = "7.1";
-        longerBBQ.carbohydrates = "33.3";
+        longerBBQ.price = 50;
+        longerBBQ.weight = 107;
+        longerBBQ.calories = 236;
+        longerBBQ.protein = 9.7;
+        longerBBQ.fats = 7.1;
+        longerBBQ.carbohydrates = 33.3;
 
         System.out.println(longerBBQ);
     }
